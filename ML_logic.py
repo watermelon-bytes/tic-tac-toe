deb079466e4b54bfce62392aa587b23ee39e255e
watermelon-bytes <ximillea.qinth@gmail.com>
2025-04-03 18:27:33 +0300

    some changes

--- ML_logic.py
+++ ML_logic.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+
 content = pd.read_csv('data.txt')
 content = content.dropna(axis=0)
 
@@ -14,3 +15,4 @@ X = [pos, turn]
 y = answer
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.02, random_state=42)
+print('Done!')
--- script.js
+++ script.js
@@ -1,201 +1,322 @@
-import { set_cell_coordinate, all_cells, drawField, onload_starting  } from './utils.js';
+let onload_starting = () => {
+  const playButton = document.createElement('button');
+  playButton.setAttribute('id', 'playbutton');
+  playButton.textContent = 'Play';
+  playButton.setAttribute('class', 'btn-primary');
+  playButton.style.position = 'absolute';
+  playButton.style.top = '50%';
+  playButton.style.left = '50%';
+  playButton.style.transform = 'translate(-50%, -50%)';
+  playButton.addEventListener('click', () => {
+    startToPlay().catch(console.error);
+  })
+  document.body.appendChild(playButton);
+}
+
+window.load = onload_starting();
+
+function setCellsCoordinates(i) { // здесь мы по цифре i вычисляем координаты ячейки
+  let coordinate = '';
+  switch (Math.ceil(i / 3)) {
+    case 1:
+      coordinate += 'c';
+      break;
+    case 2:
+      coordinate += 'b';
+      break;
+    case 3: 
+      coordinate += 'a';
+      break;
+  }
+  switch (i % 3) {
+    case 1:
+      coordinate += '3';
+      break;
+    case 2:
+      coordinate += '2';
+      break;
+    case 0:
+      coordinate += '1';
+      break;
+  }
+  return coordinate;
+}
+
+class Sprite {
+  constructor(image_style) {
+    this.title = image_style;
+    this.images_src = 'game_sprites/' + image_style + '/';
+    this.x_src = this.images_src + 'crestick.png';
+    this.o_src = this.images_src + 'zero.png';
+  }
+}
+
+let gothic = new Sprite('gothic');
+let classical = new Sprite('classical');
+
+function drawField(sideOfField) {
+
+  // Создаем контейнер для игрового поля
+  const gameContainer = document.createElement('div');
+  gameContainer.setAttribute('id', 'game-field');
+  gameContainer.setAttribute('class', 'game-container');
+  document.body.appendChild(gameContainer);
+
+  // Рассчитываем размеры ячеек
+  const cellSize = Math.min(window.innerWidth, window.innerHeight) / (sideOfField + 1) / 1.5;
+
+  // Создаем ячейки внутри контейнера
+  for (let i = 1; i <= sideOfField ** 2; i++) {
+    const cell = document.createElement('div');
+    cell.setAttribute('class', 'cell');
+    cell.setAttribute('id', setCellsCoordinates(i));
+
+    const row = Math.ceil(i / sideOfField);
+    const column = (i - 1) % sideOfField + 1;
+
+    cell.style.position = 'absolute';
+    cell.style.width = `${cellSize}px`;
+    cell.style.height = `${cellSize}px`;
+    cell.style.top = `${(row - 1) * cellSize}px`;
+    cell.style.left = `${(column - 1) * cellSize}px`;
+
+    gameContainer.appendChild(cell);
+  }
+  gameContainer.style.position = 'relative';
+  gameContainer.style.width = `${cellSize * sideOfField}px`;
+  gameContainer.style.height = `${cellSize * sideOfField}px`;
+  gameContainer.style.margin = '0 auto'; // Центрируем контейнер
+}
+
+let tryToRemove = function(button_id) {
+  if (document.getElementById(button_id)) {
+    document.getElementById(button_id).remove();
+  }
+}
+
+let all_cells = () => document.querySelectorAll('.cell'); // функция querySelectorAll()ё возвращает статический список всех элементов с указанным в аргументе аттрибутами в контейнере,указанном перед точкой. 
+let checkIsTurnPossible = (this_position) => (position[this_position] === null && !document.getElementById(this_position).hasChildNodes());
+// проверяем, можно ли сделать ход в эту клетку
 
 
 let player = null; // крестик = true, нолик = false. пока не выбрано, null.
 let turn = true; // аналогично
+let position = {};
 
-let position = {}; 
+async function startToPlay() {
+  player = null;
+  turn = true;  // Всегда начинаем с крестиков
+  tryToRemove('playbutton');
 
-async function start_to_play() {
-    player = null;
-    turn = true;  // Всегда начинаем с крестиков
-    
-    all_cells().forEach(cell => {
-        cell.remove();
+  all_cells().forEach(cell => {
+    cell.remove()
+  });
+  
+
+  for (let i = 1; i < 10; i++) {
+    position[setCellsCoordinates(i)] = null;
+  }
+
+  console.log('Waiting for choice...');
+  if (development_mode) {
+    create_turns_log();
+  }
+
+  let choice = await setChoice();
+  console.log('Choice:', choice); // проверка
+  drawField(3);
+  cellEventListener();
+}
+
+let setChoice = function() {
+  let createChoiceButton = function(id, text) {
+    const button = document.createElement('button');
+    button.setAttribute('id', id);
+    stylization(id, 'sec');
+    button.textContent = text;
+    button.addEventListener('click', () => {
+      document.getElementById('container').remove();
+    });
+    return button;
+  }
+
+  return new Promise((resolve) => {
+    const container = document.createElement('div');
+    container.setAttribute('id', 'container');
+    document.body.appendChild(container);
+
+    const promptText = 'Play as ';
+    container.appendChild(createChoiceButton('x_btn', promptText + 'X')).addEventListener('click', () => {
+      player = true;
+      resolve(true);
     });
 
-
-    if (document.getElementById('playbutton')) {
-      document.getElementById('playbutton').remove();
-    }
-  
-    for (let i = 0; i < 9; i++) {
-      position[set_cell_coordinate(i)] = null;
-    }
-    
-
-    console.log('Waiting for choice...');
-    
-    create_turns_log();
-
-    let choice = await setChoice();
-    console.log('Choice:', choice); // проверка
-    drawField();
-    cellEventListener();
-}
-
-function setChoice() {
-  return new Promise((resolve) => { // Promise - это объект, который используется для выполнения асинхронных операций и возвращения их результата в будущем.
-      let a = 'Play as ';
-      const container = document.createElement('div');
-      container.setAttribute('id', 'container');
-      document.body.appendChild(container);
-
-      x = document.createElement('button');
-      x.setAttribute('id', 'x_btn');
-      x.textContent = a + 'X';
-
-      o = document.createElement('button');
-      o.textContent = a + 'O';
-      o.setAttribute('id', 'o_btn');
-  
-      container.appendChild(x);
-      container.appendChild(o); // добавляем кнопки для выбора стороны
-
-      x.addEventListener('click', () => {
-        container.remove();
-        player = true;
-        resolve(true);
-      });
-  
-      o.addEventListener('click', () => {
-        container.remove();
-        player = false;
-        resolve(false);
-      });
+    container.appendChild(createChoiceButton('o_btn', promptText + 'O')).addEventListener('click', () => {
+      player = false;
+      resolve(false);
+    });
   });
-}
-
-var checkIsTurnPossible = function(this_position) { // проверяем, можно ли сделать ход в эту клетку
-  return (position[this_position] === null && !document.getElementById(this_position).hasChildNodes());
 }
 
 let development_mode = false
 
-function fillCell(cell_position, current_turn) { 
-    let side = document.createElement('img');
-    let the_cell = document.getElementById(cell_position);
-    
-    if (current_turn === true) {
-        side.setAttribute('src', 'crestick.png');
-        side.alt = 'X';
-    } else {
-        side.setAttribute('src', 'zero.png');
-        side.alt = '0';
-    }
-    
-    if (development_mode) {write_turn_down(cell_position);} // если всё нормально, записываем позицию + ход в log, чтобы обучать потом на партиях бота
-    position[cell_position] = current_turn;  // Записываем ход
-    the_cell.appendChild(side);
-    
-    let winner = checkForWinner();
-    if (winner !== null || checkForDraw(position)) {
-        end_game();
-    }
+function fillCell(cell_position, current_turn) {
+  let side = document.createElement('img');
+  let the_cell = document.getElementById(cell_position);
 
-    turn = !turn;
-    console.log("Check for win:", winner);
-    console.log('Position:', position); // проверка
+  if (current_turn) { 
+    side.setAttribute('src', gothic.x_src);
+    side.alt = 'X';
+  } else {
+    side.setAttribute('src', gothic.o_src);
+    side.alt = '0';
+  }
+
+  if (document.getElementById('turns_log')) {
+    write_turn_down(cell_position);
+  }
+
+  position[cell_position] = current_turn;
+  the_cell.appendChild(side);
+
+  let winner = checkForWinner();
+  if (winner !== null || checkForDraw(position)) {
+    endCurrentGame(winner);
+    return;
+  }
+
+  // Меняем ход только если игра не закончена
+  turn = !turn;
+  console.log("Ход игрока:", turn ? "X" : "O");
+  console.log('Position:', position);
 }
 
-function handler(object) {
-    if (object.hasChildNodes() === false) { // проверка на дочерние узлы 
-      fillCell(object.id, turn); // здесь мы через cell.id будем получать координаты клетки
-    }
-}
+
+/*function handler(object) {
+  if (object.hasChildNodes() === false) { // проверка на дочерние узлы 
+    fillCell(object.id, turn); // здесь мы через cell.id будем получать координаты клетки
+  }
+}*/
 
 function cellEventListener() {
-    all_cells().forEach(cell => { // здесь cell - это название переменной, которая будет принимать значение каждой клетки
-      
-      cell._clickHandler = () => {
-        if (checkIsTurnPossible(cell.id)) {
-            fillCell(cell.id, turn);
-        }
-    };
+  all_cells().forEach(cell => { // здесь cell - это название переменной, которая будет принимать значение каждой клетки
+
+    cell._clickHandler = () => {
+      if (checkIsTurnPossible(cell.id)) {
+        fillCell(cell.id, turn);
+      }
+    }
     cell.addEventListener('mouseup', cell._clickHandler);
   });
 }
 
-/* функция querySelectorAll()ё возвращает статический список
-всех элементов с указанным в аргументе аттрибутами в контейнере,
-указанном перед точкой. 
-hasChildNodes() возвращает true, если есть дочерние узлы, и false, если их нет.
- */
 
 function checkForWinner() {
-
   function checkForWin(game_position, player_flag) {
+    // Получаем все клетки текущего игрока
+    const playerCells = Object.keys(game_position).filter(
+      key => game_position[key] === player_flag
+    );
+    const letters = ['a', 'b', 'c'];
+    const numbers = [1, 2, 3];
 
-    // Object.keys(game_position) => Получаем массив всех ключей объекта game_position
-    cells_with_flag_id = Object.keys(game_position)
-    cells_with_flag_id = cells_with_flag_id.filter(key => game_position[key] === player_flag); // все клетки с player_flag
-    
-    /* .filter(key => game_position[key] === player_flag) => фильтруем этот массив, 
-    оставляя только те ключи, у которых значение в объекте game_position равно flag */
-
-
-    let nums = ['1', '2', '3'];
-    let letters = ['a', 'b', 'c'];
-
-    var necessary_filter = (index, elem) => {
-      return cells_with_flag_id.filter(cell_code => cell_code[index] == elem).length === 3;
+    for (let letter of letters) {
+      if (numbers.every(number => playerCells.includes(letter + number))) {
+        return true;
+      }
     }
-    
-    for (let first of letters) {
-      if (necessary_filter(0, first)) {
+    for (let number of numbers) {
+      if (letters.every(letter => playerCells.includes(letter + number))) {
         return true;
       }
     }
 
-    for (let second of nums) {
-      if (necessary_filter(1, second)) {
-        return true;
-      }
-    }
-
-    /*
-    for (let element of cells_with_flag_id) {
-      if 
-    } */
+    const diagonals = [['a1', 'b2', 'c3'], ['a3', 'b2', 'c1']];
+    return diagonals.some( diagonal => diagonal.every(cell => playerCells.includes(cell)) ) ? true : false;
   }
-  let out;
-  checkForWin(position, true) ? out = 'X wins!' : checkForWin(position, false) ? out = 'O wins!' : out = null;
-  return out;
+
+  if (checkForWin(position, true)) {
+    return 'X wins!';
+  } else if (checkForWin(position, false)) {
+    return 'O wins!';
+  }
+  return null;
 }
 
+var checkForDraw = (this_position) => {
+  const values = Object.values(this_position);
+  if (values.every(cell => cell !== null) && checkForWinner() === null) {
+    console.info('Draw!');
+    return true;
+  }
+  return false;
+}
 
-
-var checkForDraw = function(this_position) {
-    const values = Object.values(this_position);
-    if (values.every(cell => cell !== null) && checkForWinner() === null) {
-        console.info('Draw!');
-        return true;
-    }
-    return false;
+function stylization(elem_id, type) {
+  let btn_type;
+  switch (type.slice(0, 3)) {
+    case 'pri':
+      btn_type = 'btn-primary';
+      break;
+    case 'sec':
+      btn_type = 'btn-secondary';
+      break;
+    default:
+      btn_type = 'btn-primary';
   }
 
+  try {
+    elem = document.getElementById(elem_id);
+    elem.setAttribute('class', btn_type);
+  } catch (e) {
+    console.error(e);
+  }
+}
 
+async function endCurrentGame(winner) {
+  all_cells().forEach(cell => {
+    cell.removeEventListener('mouseup', cell._clickHandler);
+  });
 
-async function end_game() {
+  if (winner !== null) {
+    console.info(winner);
+  } else {
+    console.info('Draw!');
+  }
 
-  all_cells().forEach(cell => {cell.removeEventListener('mouseup', cell._clickHandler);});
+  let _playAgainButton_ = document.createElement('button');
+  _playAgainButton_.innerHTML = 'Play again?';
+  _playAgainButton_.setAttribute('id', 'play_again_btn');
+  stylization('play_again_btn', 'prim');
+  
+  let gameField = document.getElementById('game-field');
+  let gameFieldRect = gameField.getBoundingClientRect();
+  
+  _playAgainButton_.style.position = 'absolute';
+  _playAgainButton_.style.top = `${gameFieldRect.bottom + 10}px`;
+  _playAgainButton_.style.left = `${gameFieldRect.left}px`;
+  document.body.appendChild(_playAgainButton_);
 
+  _playAgainButton_.addEventListener('click', () => {
+    tryToRemove('play_again_btn');
+    tryToRemove('game-field');
+    startToPlay().catch(console.error);
+  });
+  
   console.info('Game over!');
-  alert('Game over!');
-  start_to_play();
 }
 
-function create_turns_log() {
-  const container = document.createElement('div');
+var create_turns_log = function() {
+  let container = document.createElement('div');
   container.setAttribute('id', 'turns_log');
-  container.setAttribute('position', 'absolute')
-  container.setAttribute('left', '50%');
-  container.setAttribute('top', '5%');
+  container.setAttribute('position', 'absolute');
+  container.left = '50%';
+  container.top = '5%';
   document.body.appendChild(container);
 }
 
-function write_turn_down(move) {
+var write_turn_down = function(move) {
   let text = document.createElement('p');
   document.getElementById('turns_log').appendChild(text);
-  text.innerHTML += JSON.stringify(position)+ ", " + move;
+  text.innerHTML += JSON.stringify(position) + ", " + move;
 }
